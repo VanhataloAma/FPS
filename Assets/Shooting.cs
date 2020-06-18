f0735e9082b82fce99a702ed0c97a439e17da143
Amanda <amanda.vanhatalo@tuni.fi>
2020-06-18 10:34:35 +0300

    Raycast

---
+++ Assets/Shooting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    public Camera fpsCamera;
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hitInfo))
+        {
+            Debug.Log(hitInfo.transform.name);
+        }
+    }
+}
